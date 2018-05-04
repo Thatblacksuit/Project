5f6200d8289d0280ff493ca6b29b0e39e9344f2e
Thatblacksuit <bakw1_15@uni.worc.ac.uk>
2018-05-04 14:04:28 +0100

    chartworks

--- js/Calculations.js
+++ js/Calculations.js
@@ -4,7 +4,11 @@ window.onload = function(){
     var section3 = document.getElementById('overtimeform');
     var OThours = document.getElementById('OvertimeHours');
     var OTwage = document.getElementById('OvertimeWage');
-
+    var taxtotal = 0;
+    var regtotal = 0;
+    var years = document.getElementById('year-range').value;
+    var grandtotal = 0;
+    
 if(rate == "8760"){
     section2.style.display = "block";
 } else {
@@ -59,8 +63,7 @@ var OThours = document.getElementById('OvertimeHours').value;
 var OTwage = document.getElementById('OvertimeWage').value;
 var years = document.getElementById('year-range').value;
     
-var taxtotal = 0;
-var regtotal = 0;
+
 var OTtotal = OTwage * OThours;
     
 if (rate == 8760){
@@ -69,7 +72,7 @@ if (rate == 8760){
     regtotal = (salary * rate);
 }   
 
-if (years == 8){
+if (years == 2011){
    if (regtotal <= 35000) {
     taxtotal = regtotal * 0.8;
 } else if (regtotal > 35000 && regtotal <= 150000){
@@ -79,7 +82,7 @@ if (years == 8){
 } 
 }
     
-    else if (years == 7){
+    else if (years == 2012){
     if (regtotal <= 34370) {
     taxtotal = regtotal * 0.8;
 } else if (regtotal > 34370 && regtotal <= 150000){
@@ -89,7 +92,7 @@ if (years == 8){
 }   
 }    
     
-        else if (years == 6){
+        else if (years == 2013){
     if (regtotal <= 32010) {
     taxtotal = regtotal * 0.8;
 } else if (regtotal > 32010 && regtotal <= 150000){
@@ -98,7 +101,7 @@ if (years == 8){
     taxtotal = regtotal * 0.5;
 }   
 }
-        else if (years == 5){
+        else if (years == 2014){
     if (regtotal <= 31865) {
     taxtotal = regtotal * 0.8;
 } else if (regtotal > 31865 && regtotal <= 150000){
@@ -108,7 +111,7 @@ if (years == 8){
 }   
 }
     
-        else if (years == 4){
+        else if (years == 2015){
     if (regtotal <= 31785) {
     taxtotal = regtotal * 0.8;
 } else if (regtotal > 31785 && regtotal <= 150000){
@@ -118,7 +121,7 @@ if (years == 8){
 }   
 }
     
-        else if (years == 3){
+        else if (years == 2016){
     if (regtotal <= 32000) {
     taxtotal = regtotal * 0.8;
 } else if (regtotal > 32000 && regtotal <= 150000){
@@ -128,7 +131,7 @@ if (years == 8){
 }   
 }
     
-        else if (years == 2){
+        else if (years == 2017){
     if (regtotal <= 33500) {
     taxtotal = regtotal * 0.8;
 } else if (regtotal > 33500 && regtotal <= 150000){
@@ -149,8 +152,8 @@ if (regtotal <= 34500) {
 }
     
     
-var grandtotal = regtotal + OTtotal;   
+grandtotal = regtotal + OTtotal;   
     window.alert("Your salary yearly salary before tax is " + regtotal +"\n\nYour salary this year including overtime is :" + grandtotal +"\n\nYour total salary after tax: " + taxtotal);
-    window.location.reload();
+
 };
 
